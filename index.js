309ae2c8e51a861bbe1e4f2fd9fa065612830442
Nick <nprice0502@yahoo.com>
2023-06-15 18:16:25 -0400

    added content to index.js but dont know if index is even needed, alsocreated sql file

---
+++ index.js
@@ -0,0 +1,50 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+// Function to display the main menu and handle user input
+function displayMainMenu() {
+    inquirer
+      .prompt({
+        name: 'choice',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit'
+        ]
+      })
+      .then((answer) => {
+        switch (answer.choice) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            connection.end();
+            break;
+        }
+      });
+  }
