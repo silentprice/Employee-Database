395a1b1599b485bc1f617aaf202928c9b590a65a
Nick <nprice0502@yahoo.com>
2023-06-25 18:14:48 -0400

    created seeds

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (001, "Prick", "Nice",""),
+       (002, "Tyler", "Yeswak", ""),
+       (003, "Drew", "Dombro", ""),
+       (004, "Chuck", "Nawblock", ""),
+       (005, "Randy", "Pepper", ""),
+       (006, "Mickey", "Gold", ""),
+       (007, "Cali" "Moore", "");
+
+INSERT INTO role ( id, title, salary, department_id)
+VALUES (001,"Head-Programmer", "260000",""),
+       (002, "CMO", "150000",""),
+       (003, "Sales Manager","150000",""),
+       (004, "Delivery Coord", "120000", ""),
+       (005, "Product Manager", "140000", ""),
+       (006, "COO", "160000", ""),
+       (007, "CTO", "175000", "");
