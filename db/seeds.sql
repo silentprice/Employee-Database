328e4ff7a6bdd18a4ea17da2bf8abf0f55a9d8cf
Nick <nprice0502@yahoo.com>
2023-06-25 18:28:17 -0400

    finished writing the seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,26 @@
 INSERT INTO employee (id, first_name, last_name, role_id)
-VALUES (001, "Prick", "Nice",""),
-       (002, "Tyler", "Yeswak", ""),
-       (003, "Drew", "Dombro", ""),
-       (004, "Chuck", "Nawblock", ""),
-       (005, "Randy", "Pepper", ""),
-       (006, "Mickey", "Gold", ""),
-       (007, "Cali" "Moore", "");
+VALUES (001, "Prick", "Nice","11"),
+       (002, "Tyler", "Yeswak", "22"),
+       (003, "Drew", "Dombro", "33"),
+       (004, "Chuck", "Nawblock", "44"),
+       (005, "Randy", "Pepper", "55"),
+       (006, "Mickey", "Gold", "77"),
+       (007, "Cali" "Moore", "99");
 
 INSERT INTO role ( id, title, salary, department_id)
-VALUES (001,"Head-Programmer", "260000",""),
-       (002, "CMO", "150000",""),
-       (003, "Sales Manager","150000",""),
-       (004, "Delivery Coord", "120000", ""),
-       (005, "Product Manager", "140000", ""),
-       (006, "COO", "160000", ""),
-       (007, "CTO", "175000", "");
+VALUES (001,"Head-Programmer", "260000","100"),
+       (002, "CMO", "150000","200"),
+       (003, "Sales Manager","150000","300"),
+       (004, "Delivery Coord", "120000", "400"),
+       (005, "Product Manager", "140000", "500"),
+       (006, "COO", "160000", "700"),
+       (007, "CTO", "175000", "900");
+
+INSERT INTO department (id)
+VALUES (1),
+       (2),
+       (3),
+       (4),
+       (5),
+       (7),
+       (9);
